5dc215b5cf3d765cc28c0953ddf59a20fe2fc6ec
Logan Coats <logancoats222@gmail.com>
2022-04-04 14:44:44 -0500

    part 8(8)

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var housesRouter = require('./routes/houses');
 var usersRouter = require('./routes/users');
 var addmodsRouter = require('./routes/addmods')
+var selectorRouter = require('./routes/selector')
 var app = express();
 
 // view engine setup
@@ -24,6 +25,7 @@ app.use('/', indexRouter);
 app.use('/houses', housesRouter);
 app.use('/users', usersRouter);
 app.use('/addmods', addmodsRouter)
+app.use('/selector',selectorRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
